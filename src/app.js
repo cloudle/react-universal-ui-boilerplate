035de9242543d94345f967af440caa354aed9973
cloud <lehaoson@gmail.com>
2017-02-07 16:37:13 +0700

    Upgrade react-universal-ui version.

--- src/app.js
+++ src/app.js
@@ -11,23 +11,21 @@ const { isIos, isAndroid } = utils;
 
 export default class app extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit src/app.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <Button
-          wrapperStyle={{backgroundColor: '#00bcd4', width: 120}}
-          title="Click me!!" onPress={() => alert("Yay!")}/>
-      </View>
-    );
+    return <View style={styles.container}>
+      <Text style={styles.welcome}>
+        Welcome to React Native!
+      </Text>
+      <Text style={styles.instructions}>
+        To get started, edit src/app.js
+      </Text>
+      <Text style={styles.instructions}>
+        Press Cmd+R to reload,{'\n'}
+        Cmd+D or shake for dev menu
+      </Text>
+      <Button
+        wrapperStyle={{backgroundColor: '#00bcd4', width: 120}}
+        title="Click me!!" onPress={() => alert("Yay!")}/>
+    </View>
   }
 }
 
