e08cc94cc33a7adba393ee7756f6f7aa4c9ec2a1
cloudle <lehaoson@gmail.com>
2018-02-09 20:17:42 +0700

    Lock webpack-dev-server version, clean up code ;)

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 import { AppRegistry } from 'react-native';
-import { utils } from 'react-universal-ui';
 import App from './src';
 
 AppRegistry.registerComponent('app', () => App);
-
-if (utils.isBrowser) {
-	AppRegistry.runApplication('app', {
-		initialProps: {},
-		rootTag: document.getElementById('root'),
-	});
-}
